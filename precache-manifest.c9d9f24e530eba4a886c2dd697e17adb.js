167eabc0b286bcee3e82706b088e80582203ee32
Charles <cwdick@gmail.com>
2020-02-05 21:33:58 +0900

    Updates

---
+++ precache-manifest.c9d9f24e530eba4a886c2dd697e17adb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9781b39da15bc5a9b82509f463f49218",
+    "url": "/flashcard/index.html"
+  },
+  {
+    "revision": "bce27f0558d623f5f097",
+    "url": "/flashcard/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "473526810db29af35606",
+    "url": "/flashcard/static/js/2.d78dc060.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/flashcard/static/js/2.d78dc060.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bce27f0558d623f5f097",
+    "url": "/flashcard/static/js/main.68bc1a9b.chunk.js"
+  },
+  {
+    "revision": "b4c979ab05bde0798f6c",
+    "url": "/flashcard/static/js/runtime-main.34d76977.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flashcard/static/media/logo.5d5d9eef.svg"
+  }
+]);
